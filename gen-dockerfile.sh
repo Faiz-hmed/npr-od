98b701290cbf6e1bc3661a5a8d01c6a5ab7117b1
faiz-hmed <faiz.ahmeds2000@gmail.com>
2022-08-20 23:31:13 +0530

    Fixed model dir matcher pattern

--- gen-dockerfile.sh
+++ gen-dockerfile.sh
@@ -1,4 +1,4 @@
-export directory_to_add="$(ls -r | grep "[0-9]" | head -1)"
+export directory_to_add="$(ls -rd */ | grep -x -E "[0-9]/+" | head -1)"
 export model_name="NumPlateDetector"
 
 envsubst < Dockerfile.template > Dockerfile
